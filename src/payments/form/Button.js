d9d3b79d1bfdfd9708c379368e9e040cd47a8b09
Giovani de Sousa Leite <sousa_giovani@hotmail.com>
2019-02-26 18:04:54 -0300

    Make button message customizable

--- src/payments/form/Button.js
+++ src/payments/form/Button.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { Button } from '@material-ui/core'
 
-export default ({ disabled }) => (
-  <Button variant="contained" color="primary" type="submit" disabled={disabled}>
-    Finalizar
+export default ({ disabled, style, text = 'Finalizar' }) => (
+  <Button
+    variant="contained"
+    color="primary"
+    type="submit"
+    disabled={disabled}
+    style={style}
+  >
+    {text}
   </Button>
 )
